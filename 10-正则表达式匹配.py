6bd559c485a7d82ae13525113fb556c52a2de65d
金少开 <jack.jin@chalsee.com>
2022-03-08 18:08:28 +0800

    -10 -11 -12

--- 10-正则表达式匹配.py
+++ 10-正则表达式匹配.py
@@ -2,3 +2,47 @@
 # -*- coding: UTF-8 -*-
 # Filename:jinxiaojian
 # Create Time: 2022/3/3 16:30
+"""
+给你一个字符串s和一个字符规律p，请你来实现一个支持 '.'和'*'的正则表达式匹配。
+'.' 匹配任意单个字符
+'*' 匹配零个或多个前面的那一个元素
+所谓匹配，是要涵盖整个字符串s的，而不是部分字符串。
+
+示例 1：
+输入：s = "aa", p = "a"
+输出：false
+解释："a" 无法匹配 "aa" 整个字符串。
+
+示例 2:
+输入：s = "aa", p = "a*"
+输出：true
+解释：因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+
+示例3：
+输入：s = "ab", p = ".*"
+输出：true
+解释：".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+
+提示：
+1 <= s.length<= 20
+1 <= p.length<= 30
+s只包含从a-z的小写字母。
+p只包含从a-z的小写字母，以及字符.和*。
+保证每次出现字符* 时，前面都匹配到有效的字符
+
+"""
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        cache = [[False]*(len(s)+1) for _ in range(len(p)+1)]
+        cache[0][0] = True
+        for i in range(1,len(p)):
+            cache[i+1][0]=cache[i-1][0] and p[i]=='*'
+        for i in range(len(p)):
+            for j in range(len(s)):
+                if p[i]=="*":
+                    cache[i+1][j+1]=cache[i][j+1] or cache[i-1][j+1]
+                    if p[i-1]==s[j] or p[i-1]=='.':
+                        cache[i+1][j+1] |= cache[i+1][j]
+                else:
+                    cache[i+1][j+1]=cache[i][j] and (p[i]==s[j] or p[i]=='.')
+        return cache[-1][-1]
--- 11-盛最多水的容器.py
+++ 11-盛最多水的容器.py
@@ -2,3 +2,38 @@
 # -*- coding: UTF-8 -*-
 # Filename:jinxiaojian
 # Create Time: 2022/3/3 16:31
+"""
+给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+
+找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+返回容器可以储存的最大水量。
+
+示例 1：
+输入：[1,8,6,2,5,4,8,3,7]
+输出：49
+解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+示例 2：
+
+输入：height = [1,1]
+输出：1
+
+"""
+
+class Solution:
+    def maxArea(self, height):
+        left = 0
+        right = len(height) - 1
+        result = 0
+
+        while left < right:
+            water = min(height[left], height[right]) * (right - left)
+
+            if water > result:
+                result = water
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return result
